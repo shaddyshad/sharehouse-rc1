d5610b5a823c6898f714a6367b2074c666292569
shaddy shad <mwangishadrack5@gmail.com>
2018-07-27 16:43:58 +0000

    Add database connection

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 Sharehouse project backend code.
 
 # Directory structure - relative to root 
-/assets - Asset file including images, css and js file 
+/client - All client related files and templates
+    /assets - Asset file including images, css and js file 
+        /public_html - All html files will mostly belong to this directory.
+        /css - Stylesheets to most files in public_html
+        /images - Images and other graphical assets
+/server - All server side scripts and modules 
+  /src - Javascript source files
+/app.js - Server glue and application main file. It is the `start state` for the state machine proponents or the `main application` in a normal jargon.
+
+## Routes and state transitions
+All website states are routed at the route `/`, any request to this resource or any operation that may transit you to the root resource.
+`app.get('/')` - in `app.js`  
--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-//server setup
+//server setup file
 let express = require('express');
 
 let app = express();
 //server code goes here
-app.get('/', function(req, res){
-  //serve the home page 
-  res.send("Hello world!");
+
+//database connection setup
+let MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:20717/exampleDB",{auto_reconnect: true, poolSize: 10}, function(err, db){
+  if(!err){
+    console.log("We are connected.");
+    //export db to be used for subsequen connections
+  }
 })
+
+/* Execute this middleware anytime a get request mounted at `/`(root) is received
+* this will work for all requests to the index.
+*/
+app.get('/', function(req, res){
+  //The root is interpreted differently based on whether it's an anonymous request or a request from an authenticated user.
+
+  //if authenticated, call the next middleware to handle the profile pages
+  if(req.authenticated){
+    next();
+  }else{
+  // else, show the root page(index page)
+
+  res.send("Hello world!");
+},//this callback is chained at the authentication check, it will handle displaying of
+//different profile pages based on user type
+)
 
 //
 let server = app.listen(3000, function(){
