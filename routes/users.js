7a67567fdcc3afe0bc9801a5cd7db638b0963517
shaddy shad <mwangishadrack5@gmail.com>
2018-07-28 19:13:36 +0000

    add: Connect to db and declare some samples

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET a list of all users. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 
 module.exports = router;
