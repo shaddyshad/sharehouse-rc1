3028dabca371deb2670ce85ec8dd59144cb0b146
shaddy shad <mwangishadrack5@gmail.com>
2018-07-29 20:05:44 +0000

    Add POST('/user') support for user data

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,63 @@
 var express = require('express');
-var {db_connection} = require('../app.js');
+var mongoose = require('mongoose');
 
 var router = express.Router();
 
-/*DB - A minor hack around asynchronous behaviour of JS
-* Since this route might be accessed before the db connection, in the root file has been completed, it is important that we poll the connection for some time before giving up.
-* This snippet polls the connection every 1000 ms, for 5 times after which the application can assume no connection and exit with a status code 1.
-* A better mechanism will be sought.
-*/
-var connection = db_connection;
-db_config = {
-	nr_retries: 5,
-	retry_period: 1000 /*1 second = 1000ms*/
-}
-if(!connection){
-	var i = 0;
-	for(; i < db_config.nr_retries; ++i){
-		console.log("Retrying connection...: ", i);
-		setTimeout(nop, db_config.retry_period);
-	}
-	if(i >= db_config.retries){
-		console.error("Could not establish connection in time. ");
-		process.exit();
-	}
-}
-function nop() {}
+var db;
+var db_uri = "mongodb://sharehouse:Share1nsecurePWD@ds257851.mlab.com:57851/sharehouse";
+var db_options = {
+	useNewUrlParser: true
+};
+mongoose.connect(db_uri, db_options)
+.then(function(_db){
+	db = _db;
+})
+.catch(function(_err){
+	console.error("[Users] - Connection failure ",_err);
+	 process.exit(1);
+ });
+
+var userSchemaOptions = {
+	collection: "users",
+	timestamps: { createdAt: 'created_at', updatedAt: 'updated_at'},
+};
+
+var _schema = {
+	username: String,
+	password: String,
+	user_type: Boolean, //0 for depositor, 1 for operator
+};
+var UserSchema = mongoose.Schema(_schema, userSchemaOptions);
+var Users = mongoose.model('users', UserSchema);
+
 
 /* GET a list of all users. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	Users.find({})
+	.then(function(users){
+			let _users = JSON.stringify(users);
+			res.json(_users);
+	})
+	.catch(function(err){
+				console.error(err);
+				res.json({"status": "404", "message": "Error fetching users."})
+	});
 });
+
+/*POST a user to the database*/
+router.post('/', function(req, res, next){
+	var _userForm = req.body;
+	var userType = _userForm.user_type;
+	var _type = userType == "operator"?true:false;
+	var _user = Users.create({username: req.body.username, password: req.body.password, user_type: _type});
+	_user.then(function(user){
+		console.log("Added user: ", user);
+		res.json({"status": "200", "message": "Added  succesfully"});
+	}).catch(function(err){
+		console.error("Error adding user ",err);
+		res.json({"status":"1", "message": "Failed to add object to the db."});
+	});
+})
 
 
 module.exports = router;
