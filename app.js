48db55eb5e34707ed4f4c1c6f32f43d71296ea5b
shaddy shad <mwangishadrack5@gmail.com>
2018-07-30 06:24:39 +0000

    Add warehouse route middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var warehouseRouter = require('./routes/warehouse');
 
+
 var app = express();
 
 // view engine setup
