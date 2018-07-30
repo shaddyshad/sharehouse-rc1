be91f2217e533b3c8ac52c1f82a8e0632ddb7d05
shaddy shad <mwangishadrack5@gmail.com>
2018-07-30 16:44:50 +0000

    Fix db connection error

--- routes/database.js
+++ routes/database.js
@@ -2,13 +2,14 @@ var mongoose = require('mongoose');
 
 console.log("Setting up connection to the DB...");
 var db;
-var db_uri = "mongodb://sharehouse:Share1Share1nsecurePWD@ds257851.mlab.com:57851/sharehouse";
+var db_uri = "mongodb://sharehouse:Share1nsecurePWD@ds257851.mlab.com:57851/sharehouse";
 var db_options = {
   useNewUrlParser: true
 }
-var connection = mongoose.connect(db, db_uri);
+var connection = mongoose.connect(db_uri, db_options);
 connection.then(function(_db){
   db = _db;
+  console.log("Ready...");
   return db;
 }).catch(function(err){
   //No need to continue operation with no connection, send a SIGTERM signal, and log an error.
