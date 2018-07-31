81f44b193a3aab944c3ddcfff3ca9fc775341555
shaddy shad <mwangishadrack5@gmail.com>
2018-07-31 11:01:04 +0000

    Standalone booking routes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/warehouses', warehouseRouter);
-app.use('/warehouses/booking', bookingRouter);
+app.use('/bookings', bookingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
