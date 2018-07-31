6d749ccd2d177badba178ebe6b075c0b274b3c07
shaddy shad <mwangishadrack5@gmail.com>
2018-07-31 14:04:20 +0000

    Improve router management

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var warehouseRouter = require('./routes/warehouse');
+var {warehouseRouter} = require('./routes/warehouse');
 var bookingRouter = require('./routes/booking');
 
 
