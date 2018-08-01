19af5c7e5cbc387a1163460a30ce3b3c92124551
shaddy shad <mwangishadrack5@gmail.com>
2018-08-01 14:49:23 +0000

    Add ejs render()

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { user: {name: "Shad"} });
+});
+
+router.get('/another', function(req, res, next){
+  res.render('search');
 });
 
 module.exports = router;
