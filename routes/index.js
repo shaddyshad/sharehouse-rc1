fc5153e5cc24a98a8ba75e14cc50611304d476c7
shaddy shad <mwangishadrack5@gmail.com>
2018-08-04 20:23:52 +0000

    Remove old files

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,19 @@ var express = require('express');
 
 var router = express.Router();
 
+router.get('/dummy', function(req, res){
+  res.render('preview');
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { user: {name: "Shad"} });
 });
 
-router.get('/another', function(req, res, next){
+router.get('/another', function(req, res){
   res.render('search');
 });
+
+
 
 module.exports = router;
