f665b29d2865f6f2a290e896561408a567e6c384
shaddy shad <mwangishadrack5@gmail.com>
2018-08-05 17:09:26 +0000

    Remove Geo-coordinates

--- models/warehouses.js
+++ models/warehouses.js
@@ -4,33 +4,17 @@ var connection = require('../routes/database.js');
 mongoose.connection = connection;
 
 var _schemaOptions = {
-    typeKey: '$type',
-    // timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'},
-    // collection: 'entries'
+    timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'},
 };
-
 var _schema = {
-    location :{
-        type: String,
-        coordinates:[]
-    },
-    name: {'$type': String},
-    size: Number,
-    price_per_sqm: Number,
-    empty_size: Number,
+    location: String,
+    name: String,
+    area: Number,
+    empty: Number,
+    price: Number,
     operator: {type: mongoose.Schema.Types.ObjectId}
 };
-
 var warehouseSchema = new mongoose.Schema(_schema, _schemaOptions);
-/*Including distance in the query select fields using a virtual field.
-  We don't want the distance to show, since it's a relative measure and there is not any absolute point of reference we can use, but distance plays a critical
-  business role when it comes to finding warehouse. Ordering the output of the query by distance, makes perfect business sense.
-*/
-
-
-
-//Compiled model
 var Warehouse = mongoose.model(process.env.DEV_WAREHOUSE_COLLECTION, warehouseSchema);
-
 
 module.exports = Warehouse;
