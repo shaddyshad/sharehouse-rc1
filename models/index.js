7478bdd1b67b5ccb7cab596e6adf1e3e87718f66
shaddy shad <mwangishadrack5@gmail.com>
2018-08-05 02:35:04 +0000

    Add support for data decoupling

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 module.exports = {
     Users: require('./users'),
+    Warehouse: require('./warehouses')
 };
--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,41 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var connection = require('./database.js')
+var connection = require('./database.js');
 
 var router = express.Router();
 
 //Database setup
 mongoose.connection = connection;
 
- var _schemaOptions = {
-   typeKey: '$type',
-   // timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'},
-   // collection: 'entries'
- };
-
- var _schema = {
-   location :{
-     type: String,
-     coordinates:[]
-   },
-   name: {'$type': String},
-   size: Number,
-   price_per_sqm: Number,
-   empty_size: Number
- };
-
-var warehouseSchema = new mongoose.Schema(_schema, _schemaOptions);
-/*Including distance in the query select fields using a virtual field.
-  We don't want the distance to show, since it's a relative measure and there is not any absolute point of reference we can use, but distance plays a critical
-  business role when it comes to finding warehouse. Ordering the output of the query by distance, makes perfect business sense.
-*/
-
-
-
-//Compiled model
-var Warehouse = mongoose.model(process.env.DEV_WAREHOUSE_COLLECTION, warehouseSchema);
-
-
+var {Warehouse} = require('../models');
 
 //APIs
 //GET a list of all warehouses
@@ -73,7 +45,7 @@ router.post('/', function(req, res, next){
   var loc = {
     type: "Point",
     coordinates: _warehouseLocation
-  }
+  };
   //Process the other fiels
   var size = warehouseForm.size;
   var empty = warehouseForm.empty;
@@ -197,5 +169,4 @@ router.get('/manage', function(req, res){
 
 
 exports.warehouseRouter = router;
-exports.Warehouse = Warehouse;
 // module.exports = router;
