3370b71c4a4ebe96689b97469b6f956dde7bfa14
shaddy shad <mwangishadrack5@gmail.com>
2018-08-06 13:47:00 +0000

    Add the landing page

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('login', { user: {name: "Shad"} });
+  req.isAuthenticated() ? res.redirect('/users/dashboard') : res.redirect('/users/login');
 });
 
 router.get('/register', function(req, res){
-  res.render('register');
+  req.isAuthenticated() ? res.redirect('/users/dashboard') : res.redirect('/users/register')
 });
 
 router.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
+
+router.get('/error', function (req, res) {
+    res.render('permission')
+})
 
 
 module.exports = router;
